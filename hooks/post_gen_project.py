534071f888936de6e91e76f8a835f653b8d59c92
WithPrecedent <coreyrayburnyung@gmail.com>
2023-08-23 14:05:46 -0500

    added ruff exclusions and descriptive comments

--- hooks/post_gen_project.py
+++ hooks/post_gen_project.py
@@ -1,1 +1,19 @@
 """Cookiecutter hook for post project generation."""
+
+
+def main() -> None:
+    """Initializes and commits repository to GitHub through shell."""
+    if '{{ cookiecutter.commit_to_github }}'.lower() == 'y':
+        import subprocess
+
+        subprocess.call(['git', 'init'])
+        subprocess.call(['git', 'add', '*'])
+        subprocess.call(['git', 'commit', '-m', 'Initial commit'])
+        subprocess.call(
+            ['git', 'remote', 'add', 'origin', '{{ cookiecutter.url }}', '.git'])
+        subprocess.call(['git', 'push', '-u', 'origin', 'main'])
+    return
+
+
+if __name__ == "__main__":
+    main()
