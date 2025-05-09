e9d9870db3b6db6f086ef6dc196d74855229b2a8
WithPrecedent <coreyrayburnyung@gmail.com>
2025-05-09 09:43:14 -0500

    fully replaced pdm with uv

--- hooks/post_gen_project.py
+++ hooks/post_gen_project.py
@@ -1,6 +1,6 @@
 """Cookiecutter hook for post project generation."""
 
-
+import json
 import pathlib
 import subprocess
 
@@ -52,7 +52,7 @@ def execute_command_with_output(command: list[str]) -> str:
     return process.stdout
 
 def create_virtual_environment(folder: str | pathlib.Path) -> None:
-    """Creates a virtual environment at '.venv' using `subprocess` and `pdm`.
+    """Creates a virtual environment at '.venv' using `subprocess`.
 
     Args:
         folder: path of repository folder.
@@ -91,7 +91,7 @@ def commit_to_git(url: str, folder: str | pathlib.Path) -> None:
     """
     name, password = get_github_login()
     repo = "{{ cookiecutter.repo_name }}"
-    public =  'public' if "{{ cookiecutter.repo_name }}".lower() in _TRUES else "private"
+    public = 'public' if "{{ cookiecutter.repo_name }}".lower() in _TRUES else "private"
     git_commands = (
         ['echo', password, '|', 'gh', 'auth', 'login', '--with-token'],
         ['git', 'ls-remote', '-h', url, '&>', '/dev/null'],
@@ -105,19 +105,37 @@ def commit_to_git(url: str, folder: str | pathlib.Path) -> None:
         # ['git', 'push', '-u', 'origin', 'main'])
     execute_commands(commands = git_commands, folder = folder)
 
-
 def build_and_deploy_docs(folder: str | pathlib.Path) -> None:
-    """Builds and deploys docs using `subprocess`, `pdm`, and `mkdocs`.
+    """Builds and deploys docs using `subprocess` and `mkdocs`.
 
     Args:
         folder: path of repository folder.
 
     """
     docs_commands = (
-        ['pdm', 'run', 'mkdocs', 'build'],
-        ['pdm', 'run', 'mkdocs', 'gh-deploy', '--force', '--clean'])
+        ['uv', 'run', 'mkdocs', 'build'],
+        ['uv', 'run', 'mkdocs', 'gh-deploy', '--force', '--clean'])
     execute_commands(commands = docs_commands, folder = folder)
 
+def reindent_cookiecutter_json():
+    """Indent .cookiecutter.json using two spaces.
+
+    The jsonify extension distributed with Cookiecutter uses an indentation
+    width of four spaces. This conflicts with the default indentation width of
+    Prettier for JSON files. Prettier is run as a pre-commit hook in CI.
+
+    Thanks to cookiecutter-uv-hypermodern-python for this code and suggestion.
+    https://github.com/bosd/cookiecutter-uv-hypermodern-python/
+
+    """
+    path = pathlib.Path(".cookiecutter.json")
+
+    with path.open() as io:
+        data = json.load(io)
+
+    with path.open(mode="w") as io:
+        json.dump(data, io, sort_keys= True, indent = 2)
+        io.write("\n")
 
 def main() -> None:
     """Executes post repository generation scripts."""
@@ -133,7 +151,7 @@ def main() -> None:
             print(
                 'Cannot deploy documentation without creating a virtual '
                 'environment')
-
+    reindent_cookiecutter_json()
 
 if __name__ == "__main__":
     main()
